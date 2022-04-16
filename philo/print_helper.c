f5686470b2eed85161f00bedfc7b186c9f26ed6c
vyumoiseenkov <vyumoiseenkov@miem.hse.ru>
2022-04-16 22:06:06 +0300

    change output

--- philo/print_helper.c
+++ philo/print_helper.c
@@ -2,10 +2,8 @@
 
 void	print_info(t_philo *philo, char *msg)
 {
-	if (!philo->info->sharable.signal)
-	{
 		pthread_mutex_lock(&philo->info->sharable.cout);
-		printf("%lu %d %s\n", get_time() - philo->info->time_of_start, philo->id, msg);
+		if (!philo->info->sharable.signal)
+			printf("%lu %d %s\n", get_time() - philo->info->time_of_start, philo->id, msg);
 		pthread_mutex_unlock(&philo->info->sharable.cout);
-	}
 }
